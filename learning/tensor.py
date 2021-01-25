7125e9c393e8daf2c2196d40dcf5979bf82d65ba
Dwight Ledet <dwightledet@gmail.com>
2021-01-25 12:43:59 +0100

    tensor

---
+++ learning/tensor.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+
+np.set_printoptions(precision=3)
+a = np.array([1, 2, 3], dtype=np.int32)
+b = [4, 5, 6]
+
+t_a = tf.convert_to_tensor(a)
+t_b = tf.convert_to_tensor(b)
+
+print(t_a)
+print(t_b)
